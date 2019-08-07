54a35ee7c44b93f22dae6a1f109ac67a61527579
Eirikur Jonsson <eirikurj@umich.edu>
2019-08-07 15:23:50 -0400

    Updating copyright date

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MDOLAB Code'
-copyright = u'2013, Everyone'
+copyright = u'2019, MDOLAB'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
