3fa71eeab79ba1cdd0cbadd923bda635c7f4d9ef
Eirikur Jonsson <36180221+eirikurj@users.noreply.github.com>
2019-11-05 16:10:02 -0500

    Update buildbot script for automatic build of docs.

--- build_all_docs.py
+++ build_all_docs.py
@@ -23,5 +23,8 @@ os.chdir(cur_dir)
 # Now build the master:
 os.system('make html')
 
+# Build depencencies that are needed in docs
+os.system('cd ../adflow/doc/ && doxygen Doxyfile && cp -r _static {0:s}/_build/html/packages/adflow/doc/'.format(cur_dir))
+
 # And copy...should only actually work on the server:
 os.system('cp -fr _build/html/* ~/doc_website_public')
