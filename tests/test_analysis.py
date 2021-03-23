c7b0dfd7050b24268fea5d21053e33f5dc1364bc
Neil Wu <neilwu@umich.edu>
2021-03-22 22:54:39 -0400

    switched to better subprocess call

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -25,26 +25,26 @@ class TestWingAnalysis(unittest.TestCase):
     def setUp(self):
         os.chdir(os.path.join(tutorialDir, "aero/analysis"))
         if not os.path.isfile("wing_vol_coarsen.cgns"):
-            subprocess.run(["cgns_utils", "coarsen", "wing_vol.cgns", "wing_vol_coarsen.cgns"], check=True)
+            subprocess.check_call(["cgns_utils", "coarsen", "wing_vol.cgns", "wing_vol_coarsen.cgns"])
         os.chdir("../")
 
     def test(self):
         # aero/geometry
         os.chdir("geometry")
-        subprocess.run(["python", "generate_wing.py"], check=True)
+        subprocess.check_call(["python", "generate_wing.py"])
         # aero/meshing/volume
         os.chdir("../meshing/volume")
         cmd = ["python", "run_pyhyp.py"]
-        subprocess.run(mpiCmd + cmd, check=True)
+        subprocess.check_call(mpiCmd + cmd)
         # aero analysis
         os.chdir("../../analysis")
         shutil.rmtree("output", ignore_errors=True)
         shutil.rmtree("output_drag_polar", ignore_errors=True)
         cmd = ["python", "aero_run.py"]
-        subprocess.run(mpiCmd + cmd + gridFlag, check=True)
+        subprocess.check_call(mpiCmd + cmd + gridFlag)
         # drag polar
         cmd = ["python", "aero_run_drag_polar.py"]
-        subprocess.run(mpiCmd + cmd + gridFlag, check=True)
+        subprocess.check_call(mpiCmd + cmd + gridFlag)
 
 
 class TestWingOpt(unittest.TestCase):
@@ -53,22 +53,22 @@ class TestWingOpt(unittest.TestCase):
         # Prepare optimization INPUT files
         # first generate FFD grids
         os.chdir("ffd")
-        subprocess.run(["python", "simple_ffd.py"], check=True)
+        subprocess.check_call(["python", "simple_ffd.py"])
         # then coarsen the grid
         os.chdir("../../aero/analysis")
         if not os.path.isfile("wing_vol_coarsen.cgns"):
-            subprocess.run(["cgns_utils", "coarsen", "wing_vol.cgns", "wing_vol_coarsen.cgns"], check=True)
+            subprocess.check_call(["cgns_utils", "coarsen", "wing_vol.cgns", "wing_vol_coarsen.cgns"])
         # go back to opt dir
         os.chdir(os.path.join(tutorialDir, "opt"))
 
     def test_ffd_parameterize(self):
         os.chdir("ffd")
         shutil.copy("../../aero/analysis/wing_vol.cgns", "wing_vol.cgns")
-        subprocess.run(["python", "parametrize.py"], check=True)
+        subprocess.check_call(["python", "parametrize.py"])
 
     def test_pyoptsparse(self):
         os.chdir("pyoptsparse")
-        subprocess.run(["python", "rosenbrock.py"], check=True)
+        subprocess.check_call(["python", "rosenbrock.py"])
 
     @unittest.skipUnless(has_SNOPT, "SNOPT is required for this test")
     def test_wing_opt_SNOPT(self):
@@ -78,7 +78,7 @@ class TestWingOpt(unittest.TestCase):
         shutil.copy("../../aero/analysis/wing_vol_coarsen.cgns", "wing_vol_coarsen.cgns")
         shutil.rmtree("output", ignore_errors=True)
         cmd = ["python", "aero_opt.py"]
-        subprocess.run(mpiCmd + cmd + gridFlag + SNOPT, check=True)
+        subprocess.check_call(mpiCmd + cmd + gridFlag + SNOPT)
 
     def test_wing_opt_IPOPT(self):
         # first copy files
@@ -87,7 +87,7 @@ class TestWingOpt(unittest.TestCase):
         shutil.copy("../../aero/analysis/wing_vol_coarsen.cgns", "wing_vol_coarsen.cgns")
         shutil.rmtree("output_IPOPT", ignore_errors=True)
         cmd = ["python", "aero_opt.py", "--output", "output_IPOPT"]
-        subprocess.run(mpiCmd + cmd + gridFlag + IPOPT, check=True)
+        subprocess.check_call(mpiCmd + cmd + gridFlag + IPOPT)
 
     @unittest.skipUnless(has_SNOPT and pyOCSM is not None, "SNOPT and pyOCSM are required for this test")
     def test_wing_opt_ESP_SNOPT(self):
@@ -97,7 +97,7 @@ class TestWingOpt(unittest.TestCase):
         shutil.copy("../../aero/analysis/wing_vol_coarsen.cgns", "wing_vol_coarsen.cgns")
         shutil.rmtree("output_ESP", ignore_errors=True)
         cmd = ["python", "aero_opt_esp.py", "--output", "output_ESP"]
-        subprocess.run(mpiCmd + cmd + gridFlag + SNOPT, check=True)
+        subprocess.check_call(mpiCmd + cmd + gridFlag + SNOPT)
 
     @unittest.skipIf(pyOCSM is None, "pyOCSM is required for this test")
     def test_wing_opt_ESP_IPOPT(self):
@@ -107,7 +107,7 @@ class TestWingOpt(unittest.TestCase):
         shutil.copy("../../aero/analysis/wing_vol_coarsen.cgns", "wing_vol_coarsen.cgns")
         shutil.rmtree("output_ESP", ignore_errors=True)
         cmd = ["python", "aero_opt_esp.py", "--output", "output_ESP"]
-        subprocess.run(mpiCmd + cmd + gridFlag + IPOPT, check=True)
+        subprocess.check_call(mpiCmd + cmd + gridFlag + IPOPT)
 
 
 class TestAirfoilOpt(unittest.TestCase):
@@ -115,10 +115,10 @@ class TestAirfoilOpt(unittest.TestCase):
         os.chdir(os.path.join(tutorialDir, "airfoilopt"))
         # mesh
         os.chdir("mesh")
-        subprocess.run(["python", "genMesh.py"], check=True)
+        subprocess.check_call(["python", "genMesh.py"])
         # FFD
         os.chdir("../ffd")
-        subprocess.run(["python", "genFFD.py"], check=True)
+        subprocess.check_call(["python", "genFFD.py"])
         os.chdir("../")
 
     @unittest.skipUnless(has_SNOPT, "SNOPT is required for this test")
@@ -128,7 +128,7 @@ class TestAirfoilOpt(unittest.TestCase):
         shutil.copy("../mesh/n0012.cgns", ".")
         shutil.rmtree("output", ignore_errors=True)
         cmd = ["python", "airfoil_opt.py"]
-        subprocess.run(mpiCmd + cmd + SNOPT, check=True)
+        subprocess.check_call(mpiCmd + cmd + SNOPT)
 
     @unittest.skipUnless(has_SNOPT, "SNOPT is required for this test")
     def test_multipoint(self):
@@ -137,7 +137,7 @@ class TestAirfoilOpt(unittest.TestCase):
         shutil.copy("../mesh/n0012.cgns", ".")
         shutil.rmtree("output", ignore_errors=True)
         cmd = ["python", "airfoil_multiopt.py"]
-        subprocess.run(mpiCmd + cmd + SNOPT, check=True)
+        subprocess.check_call(mpiCmd + cmd + SNOPT)
 
 
 class TestOverset(unittest.TestCase):
@@ -147,19 +147,21 @@ class TestOverset(unittest.TestCase):
     def test_pyhyp(self):
         os.chdir("mesh")
         cmd = ["python", "run_pyhyp.py", "--level", "L3"]
-        subprocess.run(mpiCmd + cmd, check=True)
+        subprocess.check_call(mpiCmd + cmd)
 
         # now we test the ihc check script
         cmd = ["python", "ihc_check.py", "--level", "L3"]
-        subprocess.run(cmd, check=True)
+        subprocess.check_call(cmd)
         self.assertTrue(os.path.isfile("ONERA_M6_L3_IHC.cgns"))
 
     def test_analysis(self):
         os.chdir("analysis")
         # first we need to install adflow_util
-        subprocess.run([sys.executable, "-m", "pip", "install", "git+https://github.com/DavidAnderegg/adflow_util.git"])
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "git+https://github.com/DavidAnderegg/adflow_util.git"]
+        )
         cmd = ["python", "run_adflow_L3.py"]
-        subprocess.run(mpiCmd + cmd, check=True)
+        subprocess.check_call(mpiCmd + cmd)
 
 
 if __name__ == "__main__":
