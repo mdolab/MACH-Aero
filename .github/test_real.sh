c137b51d64c6bd1a2c0905d9c86ca437e9742ef1
Neil Wu <602725+nwu63@users.noreply.github.com>
2021-08-03 14:39:00 -0400

    only run testflo for specific file changes

--- .github/test_real.sh
+++ .github/test_real.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 
-testflo -v -n 1
+CHANGED_FILES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
+echo "The following files have changed:"
+echo $CHANGED_FILES
+
+# only run MACH tests if any files under tests/ or tutorial/ have changed
+if [[ $CH == "tests/"* ]] || [[ $CH == "tutorial/"* ]]; then
+    testflo -v -n 1
+fi
