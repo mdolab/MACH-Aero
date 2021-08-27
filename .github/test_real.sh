fdc800c3d90891a5e06288b060908edfe10dec4d
Neil Wu <neilwu0626@gmail.com>
2021-08-27 10:32:00 -0400

    make sure we don't skip tests that should be tested

--- .github/test_real.sh
+++ .github/test_real.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
 
-testflo -v -n 1
+# all tests should pass on private
+if [[ $IMAGE == "private" ]] && [[ $OS == "ubuntu" ]]; then
+    EXTRA_FLAGS='--disallow_skipped'
+fi
+
+testflo -v -n 1 $EXTRA_FLAGS
