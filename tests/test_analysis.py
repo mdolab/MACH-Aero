6f0c1368cdc70c98619d2ab63d077d587ffed852
Eirikur Jonsson <eirikurj@umich.edu>
2023-06-08 07:55:49 -0400

    Adding SLSQP to tests

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -17,6 +17,7 @@ except ImportError:
 NPROCS = 2
 mpiCmd = ["mpirun", "-n", f"{NPROCS}"]
 gridFlag = ["--gridFile", "wing_vol_coarsen.cgns"]
+SLSQP = ["--opt", "SLSQP", "--optOptions", "{'MAXIT': 0}"]
 SNOPT = ["--opt", "SNOPT", "--optOptions", "{'Major iterations limit': 0}"]
 IPOPT = ["--opt", "IPOPT", "--optOptions", "{'max_iter': 0}"]
 
@@ -70,6 +71,15 @@ class TestWingOpt(unittest.TestCase):
         os.chdir("pyoptsparse")
         subprocess.check_call(["python", "rosenbrock.py"])
 
+    def test_wing_opt_SLSQP(self):
+        # first copy files
+        os.chdir("aero")
+        shutil.copy("../ffd/ffd.xyz", ".")
+        shutil.copy("../../aero/analysis/wing_vol_coarsen.cgns", "wing_vol_coarsen.cgns")
+        shutil.rmtree("output", ignore_errors=True)
+        cmd = ["python", "aero_opt.py"]
+        subprocess.check_call(mpiCmd + cmd + gridFlag + SLSQP)
+
     @unittest.skipUnless(has_SNOPT, "SNOPT is required for this test")
     def test_wing_opt_SNOPT(self):
         # first copy files
@@ -89,6 +99,15 @@ class TestWingOpt(unittest.TestCase):
         shutil.rmtree("output_IPOPT", ignore_errors=True)
         cmd = ["python", "aero_opt.py", "--output", "output_IPOPT"]
         subprocess.check_call(mpiCmd + cmd + gridFlag + IPOPT)
+
+    def test_wing_opt_ESP_SLSQP(self):
+        # first copy files
+        os.chdir("aero")
+        shutil.copy("../ffd/ffd.xyz", ".")
+        shutil.copy("../../aero/analysis/wing_vol_coarsen.cgns", "wing_vol_coarsen.cgns")
+        shutil.rmtree("output_ESP", ignore_errors=True)
+        cmd = ["python", "aero_opt_esp.py", "--output", "output_ESP"]
+        subprocess.check_call(mpiCmd + cmd + gridFlag + SLSQP)
 
     @unittest.skipUnless(has_SNOPT and ocsm is not None, "SNOPT and pyOCSM are required for this test")
     def test_wing_opt_ESP_SNOPT(self):
@@ -123,8 +142,24 @@ class TestAirfoilOpt(unittest.TestCase):
         subprocess.check_call(["python", "genFFD.py"])
         os.chdir("../")
 
+    def test_single_point_SLSQP(self):
+        os.chdir("singlepoint")
+        shutil.copy("../ffd/ffd.xyz", ".")
+        shutil.copy("../mesh/n0012.cgns", ".")
+        shutil.rmtree("output", ignore_errors=True)
+        cmd = ["python", "airfoil_opt.py"]
+        subprocess.check_call(mpiCmd + cmd + SLSQP)
+
+    def test_multipoint_SLSQP(self):
+        os.chdir("multipoint")
+        shutil.copy("../ffd/ffd.xyz", ".")
+        shutil.copy("../mesh/n0012.cgns", ".")
+        shutil.rmtree("output", ignore_errors=True)
+        cmd = ["python", "airfoil_multiopt.py"]
+        subprocess.check_call(mpiCmd + cmd + SLSQP)
+
     @unittest.skipUnless(has_SNOPT, "SNOPT is required for this test")
-    def test_single_point(self):
+    def test_single_point_SNOPT(self):
         os.chdir("singlepoint")
         shutil.copy("../ffd/ffd.xyz", ".")
         shutil.copy("../mesh/n0012.cgns", ".")
@@ -133,7 +168,7 @@ class TestAirfoilOpt(unittest.TestCase):
         subprocess.check_call(mpiCmd + cmd + SNOPT)
 
     @unittest.skipUnless(has_SNOPT, "SNOPT is required for this test")
-    def test_multipoint(self):
+    def test_multipoint_SNOPT(self):
         os.chdir("multipoint")
         shutil.copy("../ffd/ffd.xyz", ".")
         shutil.copy("../mesh/n0012.cgns", ".")
