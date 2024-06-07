43a01ca692885db3a488e36885d24c1f2bf7f09a
Umar Sajjad <umar.sajjad8@gmail.com>
2024-06-07 09:15:38 +0500

    Edited plotting file to cater for all the optimizers. Added a main function

--- tutorial/opt/aero/plot_optHist.py
+++ tutorial/opt/aero/plot_optHist.py
@@ -3,49 +3,90 @@ import argparse
 import matplotlib.pyplot as plt
 from pyoptsparse import History
 
-plt.rcParams["text.usetex"] = True  # Comment out if latex installation is not present
-plt.rcParams["font.family"] = "serif"
-plt.rcParams["font.size"] = 20
-plt.rcParams["xtick.labelsize"] = 16
-plt.rcParams["ytick.labelsize"] = 16
-plt.rcParams["lines.linewidth"] = 3.0
+def main():
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--histFile", type=str, default="opt.hst")
-parser.add_argument("--outputDir", type=str, default="./")
-args = parser.parse_args()
+    plt.rcParams["text.usetex"] = True  # Comment out if latex installation is not present
+    plt.rcParams["font.family"] = "serif"
+    plt.rcParams["font.size"] = 20
+    plt.rcParams["xtick.labelsize"] = 16
+    plt.rcParams["ytick.labelsize"] = 16
+    plt.rcParams["lines.linewidth"] = 3.0
 
-optHist = History(args.histFile)
-histValues = optHist.getValues()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--histFile", type=str, default="opt.hst")
+    parser.add_argument("--opt", type=str, default="SLSQP", choices=["IPOPT", "SLSQP", "SNOPT"])
+    parser.add_argument("--outputDir", type=str, default="./")
+    args = parser.parse_args()
 
-fig, axes = plt.subplots(nrows=5, sharex=True, constrained_layout=True, figsize=(14, 10))
+    try:
+        if not os.path.isfile(args.histFile):
+            raise FileNotFoundError(f"History file '{args.histFile}' not found.")
+        
+        optHist = History(args.histFile)
+        histValues = optHist.getValues()
 
-axes[0].plot("nMajor", "optimality", data=histValues, label="Optimality")
-axes[0].plot("nMajor", "feasibility", data=histValues, label="Feasibility")
-axes[0].set_yscale("log")
-axes[0].axhline(1e-4, linestyle="--", color="gray")
-axes[0].annotate("Convergence Criteria", xy=(3, 9e-5), ha="left", va="top", fontsize=24, color="gray")
-axes[0].legend(fontsize=20, labelcolor="linecolor", loc="upper right", frameon=False)
-axes[0].autoscale(enable=True, tight=True)
+        if args.opt == "SNOPT":
 
-axes[1].plot("nMajor", "obj", data=histValues, label="Objective")
-axes[1].set_ylabel("Objective \n($C_D$)", rotation="horizontal", ha="right", fontsize=24)
+            _, axes = plt.subplots(nrows=5, sharex=True, figsize=(10, 14))
 
-axes[2].plot("nMajor", "alpha_fc", data=histValues, label="Alpha")
-axes[2].set_ylabel(r"$\alpha [^\circ]$", rotation="horizontal", ha="right", fontsize=24)
+            # Objective Optimality and Feasibility
+            axes[0].plot("nMajor", "optimality", data=histValues, label="Optimality")
+            axes[0].plot("nMajor", "feasibility", data=histValues, label="Feasibility")
+            axes[0].set_yscale("log")
+            axes[0].axhline(1e-4, linestyle="--", color="gray")
+            axes[0].annotate("Convergence Criteria", xy=(3, 9e-5), ha="left", va="top", fontsize=24, color="gray")
+            axes[0].legend(fontsize=20, labelcolor="linecolor", loc="upper right", frameon=False)
+            axes[0].autoscale(enable=True, tight=True)
 
-axes[3].plot("nMajor", "twist", data=histValues, label="Twist")
-axes[3].set_ylabel(r"Twist $[^\circ]$", rotation="horizontal", ha="right", fontsize=24)
+            # Objective (Drag Coefficient)
+            axes[1].plot("iter", "obj", data=histValues, label="Objective ($C_D$)")
+            axes[1].set_ylabel("Objective ($C_D$)", rotation="horizontal", ha="right", fontsize=24)
 
-axes[4].plot("nMajor", "cl_con_fc", data=histValues, label="cl_con")
-axes[4].set_ylabel("Lift constraint", rotation="horizontal", ha="right", fontsize=24)
+            # Angle of Attack
+            axes[2].plot("iter", "alpha_wing", data=histValues, label="Angle of Attack")
+            axes[2].set_ylabel(r"$\alpha$ [$^\circ$]", rotation="horizontal", ha="right", fontsize=24)
 
-for ax in axes:
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
+            # Wing Twist
+            axes[3].plot("iter", "twist", data=histValues, label="Wing Twist")
+            axes[3].set_ylabel("Twist [$^\circ$]", rotation="horizontal", ha="right", fontsize=24)
 
-    # Drop the rest of the spines
-    ax.spines["left"].set_position(("outward", 12))
-    ax.spines["bottom"].set_position(("outward", 12))
+            # Lift Constraint
+            axes[4].plot("iter", "cl_con_wing", data=histValues, label="Lift Constraint")
+            axes[4].set_ylabel("Lift Constraint", rotation="horizontal", ha="right", fontsize=24)
 
-plt.savefig(os.path.join(args.outputDir, "aero_wing_opt_hist.png"))
+        else:
+
+            _, axes = plt.subplots(nrows=4, sharex=True, figsize=(10, 14))
+
+            # Objective (Drag Coefficient)
+
+            axes[0].plot("iter", "obj", data=histValues, label="Objective ($C_D$)")
+            axes[0].set_ylabel("Objective ($C_D$)", rotation="horizontal", ha="right", fontsize=24)
+
+            # Angle of Attack
+            axes[1].plot("iter", "alpha_wing", data=histValues, label="Angle of Attack")
+            axes[1].set_ylabel(r"$\alpha$ [$^\circ$]", rotation="horizontal", ha="right", fontsize=24)
+
+            # Wing Twist
+            axes[2].plot("iter", "twist", data=histValues, label="Wing Twist")
+            axes[2].set_ylabel("Twist [$^\circ$]", rotation="horizontal", ha="right", fontsize=24)
+
+            # Lift Constraint
+            axes[3].plot("iter", "cl_con_wing", data=histValues, label="Lift Constraint")
+            axes[3].set_ylabel("Lift Constraint", rotation="horizontal", ha="right", fontsize=24)
+
+        # Adjust subplot aesthetics
+        for ax in axes:
+            ax.spines["right"].set_visible(False)
+            ax.spines["top"].set_visible(False)
+            ax.spines["left"].set_position(("outward", 12))
+            ax.spines["bottom"].set_position(("outward", 12))
+
+        plt.show()
+        plt.savefig(os.path.join(args.outputDir, "aero_wing_opt_hist.jpg"), bbox_inches = 'tight')
+    
+    except FileNotFoundError as fnf_error:
+        print(fnf_error)
+
+if __name__ == "__main__":
+    main()
