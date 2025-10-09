a6f827f70d4c4d5cdbcba829bf1edf68c340b5e9
Safa Bakhshi <bakhshi@umich.edu>
2025-10-09 16:18:20 -0400

    Fix tests again

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -16,7 +16,7 @@ except ImportError:
 NPROCS = 2
 mpiCmd = ["mpirun", "-n", f"{NPROCS}"]
 gridFlag = ["--gridFile", "wing_vol_L4.cgns"]
-gridFlagOpt = ["--gridFile", "wing_vol_L4.cgns"]
+gridFlagOpt = ["--gridFile", "../../aero/analysis/wing_vol_L4.cgns"]
 optimizer = {
     "SLSQP": ["--opt", "SLSQP", "--optOptions", "{'MAXIT': 0}"],
     "SNOPT": ["--opt", "SNOPT", "--optOptions", "{'Major iterations limit': 0}"],
